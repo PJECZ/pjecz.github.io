cf8ae202bcd5edbac7353503516e6d55db83c8e8
Guillermo Valdés Lozano <guillermo.valdes@pjec.gob.mx>
2020-06-02 16:11:45 -0500

    Nueva versión de la Agenda, dos.

--- js/consultas-agenda-audiencias.js
+++ js/consultas-agenda-audiencias.js
@@ -1,11 +1,11 @@
 var dataSet = [];
-var API_KEY = '';  
+var API_KEY = '';
 var CLIENT_ID = '';
 var SCOPE = '';
 var SHEET_AUTORIDADES_ID = '';
 var RANGE_AUTORIDADES = '';
 
-$(document).ready(function(){   
+$(document).ready(function(){
 
     $.getJSON("https://storage.googleapis.com/pjecz-consultas/consultas-agenda-audiencias-llaves.json", function(datos) {
         $.each(datos, function(clave, dato) {
@@ -18,7 +18,7 @@ $(document).ready(function(){
     });
 
     CargarApiGoogleSheet();
-    
+
     LlenarTabla();
 
     let añosSelect =  $("#añosSelect");
@@ -44,32 +44,32 @@ $(document).ready(function(){
         var val = $(this).val();
         autoridadSelect.empty();
         autoridadSelect.append('<option selected="true" disabled>- Elija la Entidad o el Juzgado -</option>');
-        autoridadSelect.prop("selectedIndex", 0);  
+        autoridadSelect.prop("selectedIndex", 0);
         LLenarJuzgados(val);
     });
 
-    $('#autoridadSelect').change(function(){       
+    $('#autoridadSelect').change(function(){
         LlenarAño();
     });
 
-    $('#mostrarButton').click(function(){  
+    $('#mostrarButton').click(function(){
         $('#listaAgendaAudiencias').DataTable().clear();
-        $('#listaAgendaAudiencias').DataTable().destroy();        
+        $('#listaAgendaAudiencias').DataTable().destroy();
         var añosSelect = $("#añosSelect").val();
         if(añosSelect != null){
-            ConsultarInformacion();    
+            ConsultarInformacion();
         }
     });
 });
 
 function ConsultarInformacion() {
-    var params = {        
-        spreadsheetId: $('#autoridadSelect').val(),  
+    var params = {
+        spreadsheetId: $('#autoridadSelect').val(),
         range: $('#añosSelect').val()
     };
-    
+
     var request = gapi.client.sheets.spreadsheets.values.get(params);
-    request.then(function(response){                            
+    request.then(function(response){
         dataSet = [];
         var status = response.status;
         if(status == "200"){
@@ -81,45 +81,45 @@ function ConsultarInformacion() {
                 var Audiencia = datos[1];
                 var Fecha = datos[2];
                 var Actor = datos[3];
-                var Demandado = datos[4];                            
+                var Demandado = datos[4];
                 var informacion = [];
-                
-                if(Fecha == undefined){ 
-                    datos.push("   "); 
-                }                             
-                if(Expediente == undefined){ 
-                    datos.push("   "); 
-                }                             
-                if(Audiencia == undefined){ 
-                    datos.push("   "); 
-                }                             
-                if(Actor == undefined){ 
-                     datos.push("   "); 
-                }                             
-                if(Demandado == undefined){ 
-                     datos.push("   "); 
-                }                         
-                dataSet.push(datos);  
-            }                       
-            LlenarTabla();                       
+
+                if(Fecha == undefined){
+                    datos.push("   ");
+                }
+                if(Expediente == undefined){
+                    datos.push("   ");
+                }
+                if(Audiencia == undefined){
+                    datos.push("   ");
+                }
+                if(Actor == undefined){
+                     datos.push("   ");
+                }
+                if(Demandado == undefined){
+                     datos.push("   ");
+                }
+                dataSet.push(datos);
+            }
+            LlenarTabla();
         }
     },function(reason) {
         var code = reason.result.error.code;
         if(code == "400"){
-            dataSet = [];  
-            LlenarTabla();   
+            dataSet = [];
+            LlenarTabla();
         }
     });
 }
 
 function LLenarJuzgados(distrito) {
-    var params = {        
-        spreadsheetId: SHEET_AUTORIDADES_ID,  
+    var params = {
+        spreadsheetId: SHEET_AUTORIDADES_ID,
         range: RANGE_AUTORIDADES
     };
 
     var request = gapi.client.sheets.spreadsheets.values.get(params);
-    request.then(function(response){                            
+    request.then(function(response){
         dataSet = [];
         var status = response.status;
         if(status == "200"){
@@ -132,14 +132,14 @@ function LLenarJuzgados(distrito) {
                 if(distrito == idDistrito){
                     $('#autoridadSelect').append( '<option value="' + Id + '">' + Autoridad + '</option');
                 }
-            }                
+            }
         }
     },function(reason) {
         //console.error('error: ' + reason.result.error.message);
     });
 }
 
-function IniciarApi() {   
+function IniciarApi() {
     gapi.client.init({
         'apiKey': API_KEY,
         'clientId': CLIENT_ID,
@@ -148,33 +148,33 @@ function IniciarApi() {
     }).then(function(){});
 }
 
-function CargarApiGoogleSheet() {                
+function CargarApiGoogleSheet() {
     gapi.load('client:auth2', IniciarApi);
-}   
+}
 
-function LlenarAño(){    
+function LlenarAño(){
     $("#añosSelect").empty();
     $("#añosSelect").append('<option selected="true" disabled>- Elija un año </option>');
     $("#añosSelect").prop("selectedIndex", 0);
-    for(var i = 0; i < 5; i++) {
+    for(var i = 0; i < 1; i++) {
         var d = new Date();
         var n = d.getFullYear();
         var año = parseInt(n - i);
         $('#añosSelect').append('<option value="' + año + '">' + año + '</option');
     }
-}        
+}
 
-function LlenarTabla(){    
+function LlenarTabla(){
     $('#listaAgendaAudiencias').DataTable( {
         data: dataSet,
-        columns: [                        
-            { title: "Expediente" },                        
+        columns: [
+            { title: "Expediente" },
             { title: "Audiencia" },
             { title: "Fecha" },
             { title: "Actor(es)" },
             { title: "Demandado(s)" }
         ],
-        "pageLength": 10,        
+        "pageLength": 10,
         "order": 3,
         "language": {
             "lengthMenu": "Mostrar _MENU_",
