4ce24ce64de76af6ddda60aa407902fce169b4cf
Guillermo Valdés Lozano <guillermo.valdes@pjec.gob.mx>
2020-06-02 11:27:49 -0500

    Primer entrega de Agenda de Audiencias.

--- js/consultas-agenda-audiencias.js
+++ js/consultas-agenda-audiencias.js
@@ -1,14 +1,25 @@
-// Consultas Agenda de Audiencias
-$(document).ready(function() {
+var dataSet = [];
 
-    // Inicialmente se muestra los select y se oculta la lista
-    $("#elegirAgendaAudiencias").show();
-    $("#listaAgendaAudiencias").hide();
+var API_KEY = 'AIzaSyDTPTTr3prCYIM7GlVxtkIHXVbb4zUvikM';
+var CLIENT_ID = '616965235487-g5ld50subl69ncjktrr94f4i39nk0tk8.apps.googleusercontent.com';
+var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
-    // Opciones del select distrito
+var SHEET_AUTORIDADES_ID = '1J82-XGF-Y6N3SsDPX8qQFyy9kLfEAy49rUx7R3WcpSc';
+var RANGE_AUTORIDADES = 'Autoridades';
+
+$(document).ready(function(){
+
+    CargarApiGoogleSheet();
+
+    LlenarTabla();
+
+    let añosSelect =  $("#añosSelect");
+    añosSelect.append('<option selected="true" disabled>- Elija un año </option>');
+    añosSelect.prop("selectedIndex", 0);
+
     let distritoSelect = $("#distritoSelect");
     distritoSelect.empty();
-    distritoSelect.append('<option selected="true" disabled>- Elija la entidad o distrito -</option>');
+    distritoSelect.append('<option selected="true" disabled>- Elija la Entidad o el Distrito Judicial </option>');
     distritoSelect.prop("selectedIndex", 0);
     $.getJSON("/json/distritos.json", function(datos) {
         $.each(datos, function(clave, dato) {
@@ -16,60 +27,157 @@ $(document).ready(function() {
         })
     });
 
-    // Al cambiar el distrito se cambian las opciones en autoridad
-    $("#distritoSelect").change(function() {
+    let autoridadSelect = $("#autoridadSelect");
+    autoridadSelect.empty();
+    autoridadSelect.append('<option selected="true" disabled>- Elija la Entidad o el Juzgado -</option>');
+    autoridadSelect.prop("selectedIndex", 0);
+
+    $('#distritoSelect').change(function(){
         var val = $(this).val();
-        $("#autoridadSelect").html();
+        autoridadSelect.empty();
+        autoridadSelect.append('<option selected="true" disabled>- Elija la Entidad o el Juzgado -</option>');
+        autoridadSelect.prop("selectedIndex", 0);
+        LLenarJuzgados(val);
     });
 
-    // Al dar clic en el botón mostrar
-    $("#mostrarButton").click(function(){
+    $('#autoridadSelect').change(function(){
+        LlenarAño();
+    });
 
-        // Si hay un valor en autoridadSelect
-        if (!$("#autoridadSelect").is(":empty")) {
-
-            // Si es la primera vez se muestra la tabla
-            // de lo contrario hay que limpiar y destruir
-            // para que puedan cargarse otros resultados
-            if ($("#listaDeAcuerdos").is(":hidden")) {
-                $("#listaDeAcuerdos").show();
-            } else {
-                $('#listaDeAcuerdos').DataTable().clear();
-                $('#listaDeAcuerdos').DataTable().destroy();
-            }
-
-            // Cargar los datos a la tabla
-            // toma el valor de distritoSelect que es el URL al archivo JSON
-            // dicho archivo debe tener valores indicados
-            $('#listaDeAcuerdos').DataTable( {
-                "ajax": $("#autoridadSelect").val(),
-                "columns": [
-                    { "data": "Fecha" },
-                    { "data": "Expediente" },
-                    { "data": "Audencia" },
-                    { "data": "Actores" },
-                    { "data": "Demandados" }
-                ],
-                "pageLength": 50,
-                "order": [[ 0, "desc" ]],
-                "language": {
-                    "lengthMenu": "Mostrar _MENU_",
-                    "search": "Filtrar:",
-                    "zeroRecords": "Cargando información...",
-                    "info": "Página _PAGE_ de _PAGES_",
-                    "infoEmpty": "No hay registros",
-                    "infoFiltered": "(filtrados desde _MAX_ registros totales)",
-                    "oPaginate": {
-                        "sFirst": "Primero",
-                        "sLast": "Último",
-                        "sNext": "Siguiente",
-                        "sPrevious": "Anterior"
-                    }
-                }
-            }); // Cargar los datos a la tabla
-
-        } // Si hay un valor en autoridadSelect
-
-    }); // Al dar clic en el botón mostrar
-
+    $('#mostrarButton').click(function(){
+        $('#listaAgendaAudiencias').DataTable().clear();
+        $('#listaAgendaAudiencias').DataTable().destroy();
+        ConsultarInformacion();
+    });
 });
+
+function ConsultarInformacion() {
+    var params = {
+        spreadsheetId: $('#autoridadSelect').val(),
+        range: $('#añosSelect').val()
+    };
+
+    var request = gapi.client.sheets.spreadsheets.values.get(params);
+    request.then(function(response){
+        dataSet = [];
+        var status = response.status;
+        if(status == "200"){
+            var values = response.result.values;
+            var ColeccionDatos = [];
+            for (var i = 1; i < values.length; i++){
+                var datos = values[i];
+                var Expediente = datos[0];
+                var Audiencia = datos[1];
+                var Fecha = datos[2];
+                var Actor = datos[3];
+                var Demandado = datos[4];
+                var informacion = [];
+
+                if(Fecha == undefined){
+                    datos.push("   ");
+                }
+                if(Expediente == undefined){
+                    datos.push("   ");
+                }
+                if(Audiencia == undefined){
+                    datos.push("   ");
+                }
+                if(Actor == undefined){
+                     datos.push("   ");
+                }
+                if(Demandado == undefined){
+                     datos.push("   ");
+                }
+                dataSet.push(datos);
+            }
+            LlenarTabla();
+        }
+    },function(reason) {
+        var code = reason.result.error.code;
+        if(code == "400"){
+            dataSet = [];
+            LlenarTabla();
+        }
+    });
+}
+
+function LLenarJuzgados(distrito) {
+    var params = {
+        spreadsheetId: SHEET_AUTORIDADES_ID,
+        range: RANGE_AUTORIDADES
+    };
+
+    var request = gapi.client.sheets.spreadsheets.values.get(params);
+    request.then(function(response){
+        dataSet = [];
+        var status = response.status;
+        if(status == "200"){
+            var values = response.result.values;
+            for (var i = 1; i < values.length; i++){
+                var Arreglo = values[i];
+                var idDistrito = Arreglo[0];
+                var Autoridad = Arreglo[1];
+                var Id = Arreglo[2];
+                if(distrito == idDistrito){
+                    $('#autoridadSelect').append( '<option value="' + Id + '">' + Autoridad + '</option');
+                }
+            }
+        }
+    },function(reason) {
+        //console.error('error: ' + reason.result.error.message);
+    });
+}
+
+function IniciarApi() {
+    gapi.client.init({
+        'apiKey': API_KEY,
+        'clientId': CLIENT_ID,
+        'scope': SCOPE,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function(){});
+}
+
+function CargarApiGoogleSheet() {
+    gapi.load('client:auth2', IniciarApi);
+}
+
+function LlenarAño(){
+    $("#añosSelect").empty();
+    $("#añosSelect").append('<option selected="true" disabled>- Elija un año </option>');
+    $("#añosSelect").prop("selectedIndex", 0);
+    for(var i = 0; i < 5; i++) {
+        var d = new Date();
+        var n = d.getFullYear();
+        var año = parseInt(n - i);
+        $('#añosSelect').append('<option value="' + año + '">' + año + '</option');
+    }
+}
+
+function LlenarTabla(){
+    $('#listaAgendaAudiencias').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Expediente" },
+            { title: "Audiencia" },
+            { title: "Fecha" },
+            { title: "Actor(es)" },
+            { title: "Demandado(s)" }
+        ],
+        "pageLength": 10,
+        "order": 3,
+        "language": {
+            "lengthMenu": "Mostrar _MENU_",
+            "search": "Filtrar:",
+            "zeroRecords": " ",
+            "info": "Página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros",
+            "infoFiltered": "(filtrados desde _MAX_ registros totales)",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            }
+        }
+    } );
+}
