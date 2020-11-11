678b7d628ef909795980383d6a62c231241b0462
Constructor - PJECZ Dirección de Informática <constructor@pjecz.gob.mx>
2020-11-11 09:13:12 -0600

    Agregué Notaría 86 Torreón a Edictos.

--- js/consultas-edictos.js
+++ js/consultas-edictos.js
@@ -289,6 +289,7 @@ $(document).ready(function() {
         '<option value="https://storage.googleapis.com/pjecz-consultas/Edictos/Distrito%20de%20Torreon/Notaria%20Publica%2081%20Torreon/lista.json">Notaria Pública 81 Torreón</option>' +
         '<option value="https://storage.googleapis.com/pjecz-consultas/Edictos/Distrito%20de%20Torreon/Notaria%20Publica%2083%20Torreon/lista.json">Notaria Pública 83 Torreón</option>' +
         '<option value="https://storage.googleapis.com/pjecz-consultas/Edictos/Distrito%20de%20Torreon/Notaria%20Publica%2084%20Torreon/lista.json">Notaria Pública 84 Torreón</option>' +
+        '<option value="https://storage.googleapis.com/pjecz-consultas/Edictos/Distrito%20de%20Torreon/Notaria%20Publica%2086%20Torreon/lista.json">Notaria Pública 86 Torreón</option>' +
         '<option value="https://storage.googleapis.com/pjecz-consultas/Edictos/Distrito%20de%20Torreon/Notaria%20Publica%2088%20Torreon/lista.json">Notaria Pública 88 Torreón</option>' +
         '<option value="https://storage.googleapis.com/pjecz-consultas/Edictos/Distrito%20de%20Torreon/Notaria%20Publica%2089%20Torreon/lista.json">Notaria Pública 89 Torreón</option>'
 
