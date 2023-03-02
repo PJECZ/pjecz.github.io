5b3894f0a5f10304f80cd3d31cc1e66bef8dd37f
Plataforma Web <plataforma.web@pjecz.gob.mx>
2023-03-02 10:40:28 -0600

    Actualización Trámites y Servicios.

--- js/tramites-y-servicios.js
+++ js/tramites-y-servicios.js
@@ -2,21 +2,42 @@
 
 $(document).ready(function() {
     getDistritos();
+    getCemascAutoridades();
+    getdefensoriautoridades();
 
     // Validar que los select no esten vacíos
     $('#buscarForm').on('submit', function(event){
         // Validar que el distrito no este vacío
-        if($("#distritoSelect").val() !== null && $("#autoridadSelect").val() !== null){
-            this.submit();
+        //Validar Distritos
+        if($("#distritoSelect option:selected").val() !== '0'){
+            //Validar Juzgados
+            if($("#autoridadSelect option:selected").val() !== '0'){
+                // Validar CEMASC
+                if(  $("#cemascautoridadSelect").length ){
+                    if($("#cemascautoridadSelect option:selected").val() !== '0'){
+                        this.submit();
+                    }else{
+                        $("#msjError").append(`<div id='errAut' class="alert alert-danger text-center" rol="alert">Selecciona una Oficina CEMASC</div>`);
+                    }
+                    
+                }else{
+
+                    this.submit();
+                }
+
+            }else{
+                $("#msjError").append(`<div id='errAut' class="alert alert-danger text-center" rol="alert">Selecciona un Juzgado </div>`);
+            }
         }else {
             // Mostrar error
-            $("#msjError").append(`<div class="alert alert-danger text-center" rol="alert">Selecciona un distrito y un juzgado </div>`);
+            $("#msjError").append(`<div id='errDist' class="alert alert-danger text-center" rol="alert">Selecciona un Distrito</div>`);
             console.log("error")
         }
         // Que no se refresque la página al presionar Enter
         event.preventDefault();
     });
 
+    
     // Cambiar el valor del distrito y tomar el id para cargar las autoridades
     $("#distritoSelect").on('change', function() {
 
@@ -25,7 +46,7 @@ $(document).ready(function() {
 
     });
 
-     // URL de la API
+    // URL de la API
     function get_api_distritos(){
         var distritos_api_url;
         switch (location.hostname) {
@@ -52,8 +73,38 @@ $(document).ready(function() {
         }
         return autoridad_api_url;
     }
+
+    // URL de la API Autoridades CEMASC
+    function get_api_autoridades_cemasc(){
+        var autoridad_cemasc_api_url;
+        switch(location.hostname){
+            // Para desarrollo
+            case "localhost": autoridad_cemasc_api_url = 'http://localhost:8001/v2/autoridades/cemascs'; break;
+            case "127.0.0.1": autoridad_cemasc_api_url = 'http://127.0.0.1:8001/v2/autoridades/cemascs'; break;
+            case "172.30.37.233": autoridad_cemasc_api_url ='http://172.30.37.233:8001/v2/autoridades/cemascs'; break;
+            // Para producción
+            default: autoridad_cemasc_api_url = 'https://plataforma-web-api.justiciadigital.gob.mx/v2/autoridades/cemascs'; break;
+        }
+        return autoridad_cemasc_api_url;
+    }
+
+    // URL de la API Autoridades Defensoria
+    function get_api_autoridades_defensoria(){
+        var autoridad_defensoria_api_url;
+        switch(location.hostname){
+            // Para desarrollo
+            case "localhost": autoridad_defensoria_api_url = 'http://localhost:8001/v2/autoridades/defensorias'; break;
+            case "127.0.0.1": autoridad_defensoria_api_url = 'http://127.0.0.1:8001/v2/autoridades/defensorias'; break;
+            case "172.30.37.233": autoridad_defensoria_api_url ='http://172.30.37.233:8001/v2/autoridades/defensorias'; break;
+            // Para producción
+            default: autoridad_defensoria_api_url = 'https://plataforma-web-api.justiciadigital.gob.mx/v2/autoridades/defensorias'; break;
+        }
+        console.log(autoridad_defensoria_api_url)
+        return autoridad_defensoria_api_url;
+    }
+    
    
-    // Alimentar distritos 
+    // Obtener Distritos 
     function getDistritos(){
         var api_distritos = get_api_distritos();
 
@@ -62,11 +113,11 @@ $(document).ready(function() {
         .then(result => {
             $('#distritoSelect').append($('<option>', {
                 value: 0,
-                text: 'Todos'
+                text: 'Selecciona un Distrito'
             }))
             $.each(result.items,function (i, distrito) {
                 $('#distritoSelect').append($('<option>', {
-                    value: distrito["id"],
+                    value: distrito["clave"],
                     text: distrito["nombre"]
                 }));
             })
@@ -77,7 +128,7 @@ $(document).ready(function() {
     }       
 
 
-// obtener autoridad por distrito_id
+    // obtener Autoridad por distrito_id
     function getAutoridades(id){
 
         var api_url = get_api_autoridades(id);
@@ -87,7 +138,7 @@ $(document).ready(function() {
         .then(data => {
             $('#autoridadSelect').append($('<option>', {
                 value: 0,
-                text: 'Todos'
+                text: 'Selecciona un Juzgado u Oficina'
             }))
             $.each(data.items, function (i, autoridades) {
                 $('#autoridadSelect').append($('<option>', {
@@ -99,6 +150,59 @@ $(document).ready(function() {
         .catch((error) => {
             console.log(error)
         })
+    }
+
+
+    // obtener Autoridad CEMASC
+    function getCemascAutoridades(){
+
+        var api_url = get_api_autoridades_cemasc();
+        
+        fetch(api_url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            $('#cemascautoridadSelect').append($('<option>', {
+                value: 0,
+                text: 'Selecciona una Oficina CEMASC'
+            }))
+            $.each(data.items, function (i, autoridades) {
+                $('#cemascautoridadSelect').append($('<option>', {
+                    value: autoridades["clave"],
+                    text: autoridades["descripcion"]
+                }));
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+        
+    }
+
+     // obtener Autoridad Defensorias
+    function getdefensoriautoridades(){
+
+        var api_url = get_api_autoridades_defensoria()
+        
+        fetch(api_url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            $('#defensoriautoridadSelect').append($('<option>', {
+                value: 0,
+                text: 'Selecciona una Oficina de Defensoría'
+            }))
+            $.each(data.items, function (i, autoridades) {
+                $('#defensoriautoridadSelect').append($('<option>', {
+                    value: autoridades["clave"],
+                    text: autoridades["descripcion"]
+                }));
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+        
     }
 
 });
