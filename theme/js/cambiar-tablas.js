0eccacc51ae28b7bb09414e67ee810c85121c709
Plataforma Web <plataforma.web@pjecz.gob.mx>
2023-03-28 13:00:15 -0600

    Actualización Transparencia Proactiva.

--- theme/js/cambiar-tablas.js
+++ theme/js/cambiar-tablas.js
@@ -1,9 +1,8 @@
 // Cambiar tablas
-
 $(document).ready(function() {
   // A todas las tablas se les cambia su clase
   "use strict";
   $(document).ready(function () {
-    $("article table").addClass("table table-bordered table-hover");
+    $("article table").addClass("table table-bordered table-striped");
   });
 });
