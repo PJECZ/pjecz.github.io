3b84765781a1ca67451119b42e39d876053f951a
Plataforma Web <plataforma.web@pjecz.gob.mx>
2024-07-09 08:51:09 -0600

    Listados Interactivos v2

--- js/listados-interactivos.js
+++ js/listados-interactivos.js
@@ -1,195 +1,98 @@
-//
-// Listados interactivos
-//
-
 var borrarContenido = true;
 
-funcion2();
+window.addEventListener("load", function (event) {
+  /*   Selecciona todas las etiquetas UL */
+  var listas = document.getElementsByTagName("ul");
+  var N = 0;
+  for (var x = 0; x < listas.length; x++) {
+    var clases = listas[x].classList;
 
-function funcion2() {
-  window.addEventListener("load", function (event) {
-    /*   Selecciona todas las etiquetas UL */
-    var listas = document.getElementsByTagName("ul");
-    var N = 0;
-    for (var x = 0; x < listas.length; x++) {
-      var clases = listas[x].classList;
-
-      /* Si la lista no tiene ninguna clases, se le asigna la clases 'lista' */
-      if (clases.value === "") {
-        listas[x].setAttribute("id", "lista" + N);
-        listas[x].setAttribute("class", "lista");
-
-        var titulo = $("#lista" + N).prev();
-        var tituloText = titulo.text();
-
-        //titulo.addClass("titulo") ;
-        titulo.attr("id", "titulo" + N);
-
-        /*   Crear DIV de acordion */
-
-        const div = document.createElement("div");
-        div.setAttribute("id", "div" + N);
-        div.setAttribute("class", "divAcordion");
-
-        var tituloContenido = document.getElementById("titulo" + N);
-
-        tituloContenido.insertAdjacentElement("beforebegin", div);
-
-        const boton = document.createElement("button");
-        boton.setAttribute("id", "boton" + N);
-        boton.setAttribute("class", "accordion");
-
-        const panel = document.createElement("div");
-        panel.setAttribute("id", "panel" + N);
-        panel.setAttribute("class", "panel");
-
-        document
-          .getElementById("div" + N)
-          .insertAdjacentElement("afterbegin", boton);
-        document
-          .getElementById("div" + N)
-          .insertAdjacentElement("beforeend", panel);
-
-        const nodoLista = document.getElementById("lista" + N);
-        const contenidoLista = nodoLista.cloneNode(true);
-
-        const nodoTitulo = document.getElementById("titulo" + N);
-        const contenidoTitulo = nodoTitulo.cloneNode(true);
-
-        document
-          .getElementById("lista" + N)
-          .setAttribute("id", "lista_old_" + N);
-        document
-          .getElementById("titulo" + N)
-          .setAttribute("id", "titulo_old_" + N);
-
-        document
-          .getElementById("boton" + N)
-          .insertAdjacentElement("afterbegin", contenidoTitulo);
-        document
-          .getElementById("panel" + N)
-          .insertAdjacentElement("beforeend", contenidoLista);
-
-        if (borrarContenido) {
-          $("#titulo_old_" + N).remove();
-          $("#lista_old_" + N).remove();
-        }
-
-        N++;
-      } else {
-        //console.log(clases) ;
-      }
-    }
-
-    /* asignar evento click a cada boton para desplegar el panel */
-
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-      console.log(i);
-      acc[i].addEventListener("click", function () {
-        /* agrega la clase active */
-        this.classList.toggle("active");
-
-        /* oculta o muestra el panel  */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
-    }
-  });
-}
-
-function funcion1() {
-  window.addEventListener("load", function (event) {
-    /*  Selecciona todas las etiquetas H3 */
-    var contador = new Array();
-    var coleccion = document.getElementsByTagName("h3");
-    for (var x = 0; x < coleccion.length; x++) {
-      coleccion[x].setAttribute("id", "titulo" + x);
-      coleccion[x].setAttribute("class", "titulo");
-      contador[x] = x;
-    }
-
-    /*   Selecciona todas las etiquetas UL */
-    var listas = document.getElementsByTagName("ul");
-    for (var x = 0; x < listas.length; x++) {
-      listas[x].setAttribute("id", "lista" + x);
+    /* Si la lista no tiene ninguna clases, se le asigna la clases 'lista' */
+    if (clases.value === "") {
+      listas[x].setAttribute("id", "lista" + N);
       listas[x].setAttribute("class", "lista");
-    }
 
-    /*   Crear DIV de acordion */
-    for (x = 0; x < contador.length; x++) {
+      var titulo = $("#lista" + N).prev();
+
+      //titulo.addClass("titulo") ;
+      titulo.attr("id", "titulo" + N);
+
+      /*   Crear DIV de acordion */
+
       const div = document.createElement("div");
-      div.setAttribute("id", "div" + x);
+      div.setAttribute("id", "div" + N);
       div.setAttribute("class", "divAcordion");
 
-      titulo = coleccion[x];
+      const span = document.createElement("span");
+      span.setAttribute("id", "span" + N);
+      span.setAttribute("class", "toggle-icon");
 
-      titulo.insertAdjacentElement("beforebegin", div);
-    }
+      var tituloContenido = document.getElementById("titulo" + N);
 
-    for (x = 0; x < contador.length; x++) {
+      tituloContenido.insertAdjacentElement("beforebegin", div);
+      tituloContenido.insertAdjacentElement("beforeend", span);
+
       const boton = document.createElement("button");
-      boton.setAttribute("id", "boton" + x);
+      boton.setAttribute("id", "boton" + N);
       boton.setAttribute("class", "accordion");
 
       const panel = document.createElement("div");
-      panel.setAttribute("id", "panel" + x);
+      panel.setAttribute("id", "panel" + N);
       panel.setAttribute("class", "panel");
 
       document
-        .getElementById("div" + x)
+        .getElementById("div" + N)
         .insertAdjacentElement("afterbegin", boton);
       document
-        .getElementById("div" + x)
+        .getElementById("div" + N)
         .insertAdjacentElement("beforeend", panel);
 
-      const nodoLista = document.getElementById("lista" + x);
+      const nodoLista = document.getElementById("lista" + N);
       const contenidoLista = nodoLista.cloneNode(true);
 
-      const nodoTitulo = document.getElementById("titulo" + x);
+      const nodoTitulo = document.getElementById("titulo" + N);
       const contenidoTitulo = nodoTitulo.cloneNode(true);
 
-      document.getElementById("lista" + x).setAttribute("id", "lista_old_" + x);
+      document.getElementById("lista" + N).setAttribute("id", "lista_old_" + N);
       document
-        .getElementById("titulo" + x)
-        .setAttribute("id", "titulo_old_" + x);
+        .getElementById("titulo" + N)
+        .setAttribute("id", "titulo_old_" + N);
 
       document
-        .getElementById("boton" + x)
+        .getElementById("boton" + N)
         .insertAdjacentElement("afterbegin", contenidoTitulo);
       document
-        .getElementById("panel" + x)
+        .getElementById("panel" + N)
         .insertAdjacentElement("beforeend", contenidoLista);
 
       if (borrarContenido) {
-        $("#titulo_old_" + x).remove();
-        $("#lista_old_" + x).remove();
+        $("#titulo_old_" + N).remove();
+        $("#lista_old_" + N).remove();
       }
+
+      N++;
+    } else {
+      //console.log(clases) ;
     }
+  }
 
-    var acc = document.getElementsByClassName("accordion");
-    var i;
+  /* asignar evento click a cada boton para desplegar el panel */
 
-    for (i = 0; i < acc.length; i++) {
-      console.log(i);
-      acc[i].addEventListener("click", function () {
-        /* agrega la clase active */
-        this.classList.toggle("active");
+  var acc = document.getElementsByClassName("accordion");
+  var i;
 
-        /* oculta o muestra el panel  */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
-    }
-  });
-}
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      /* agrega la clase active */
+      this.classList.toggle("activo");
+
+      /* oculta o muestra el panel  */
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+});
